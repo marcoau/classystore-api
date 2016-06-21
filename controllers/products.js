c02fe46619e283feab38300c64ab626e7c182bbb
Marco Au <marco.au.321@gmail.com>
2016-06-21 23:22:40 +0800

    add get products controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,37 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const models = require('./../models');
+
+const Product = models.Product;
+
 function getProducts(req, res) {
-  return res.sendStatus(418);
+  // try parsing query
+  let query = {};
+  if(req.query.q) {
+    try {
+      query = JSON.parse(req.query.q);
+
+    } catch(e) {}
+  }
+
+  const match = {};
+  if(query.category) match.category = query.category;
+  if(query.type) match.type = query.type;
+  if(query.brand) match.brand = query.brand;
+  if(query.variant) match.variant = query.variant;
+
+  const aggregate = [
+    { $match: match },
+    { $limit: 20 }
+  ];
+
+  Product.aggregate(aggregate)
+    .exec((err, products) => {
+      if(err) return res.sendStatus(500);
+
+      return res.send({ data: products });
+    });
 }
 
 module.exports = {
